10cd34de897bfe73ce7de7b9546f92751eb04a2c
hwang0 <Henry.Wang@searshc.com>
2015-11-05 19:54:45 -0600

    test

--- awu/awu/urls.py
+++ awu/awu/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin1/', include(admin.site.urls)),
 	url(r'^test/$', 'BB.views.test', name='home'),
 ]
